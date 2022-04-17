62b36b779d48ae8f2efee676f0f55caea3c0ed95
Alex <lrl.msk@yandex.ru>
2022-04-17 13:45:19 +0300

    add Example007 нихера не пошло

---
+++ Example007/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.SetCursorPosition(10, 4);
+Console.WriteLine("+");
+
+int xa = 1, ya = 1, 
+    xb = 1, yb = 30, 
+    xc = 30, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
