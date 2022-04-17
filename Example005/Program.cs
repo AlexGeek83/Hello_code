13343030b6132209755f79953aa8a54502fddf67
Alex <lrl.msk@yandex.ru>
2022-04-17 12:38:21 +0300

    add Example004 Алгоритмесли Маша, то

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your Name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Машка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
